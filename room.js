6124ae7e8ebc7b3fc00501d6054b60366cde9842
b123400 <b123400@gmail.com>
2015-02-16 05:58:10 +0800

    added deploy function

--- room.js
+++ room.js
@@ -1,22 +1,35 @@
 var Room = function (name, lobby, io) {
   this.name = name;
   this.currentPlayerCount = 0;
-  this.playerCount = 2;
+  this.playerCount = 2; // target player count
+  this.sockets = [];
   var _this = this;
   io.on('connection', function(socket){
-    _this.currentPlayerCount++;
-    if (_this.playerCount == _this.currentPlayerCount) {
+    _this.sockets.push(socket);
+
+    if (_this.playerCount == _this.sockets.length) {
       lobby.roomStateChanged(_this);
       io.emit('start', {
         playerCount : _this.playerCount
       });
     }
 
-    socket.on('disconnect', function(){
-      _this.currentPlayerCount--;
-      if (_this.currentPlayerCount == 0) {
+    socket.on('disconnect', function () {
+      _this.sockets.splice(_this.sockets.indexOf(socket), 1); // remove this socket
+      if (_this.sockets.length == 0) {
+        io.emit('bye');
         lobby.removeRoom(_this);
       }
+    });
+
+    var lastDeploy = null;
+    socket.on('deploy', function () {
+      var now = Date.now();
+      if (now - lastDeploy < 3000) return; // prevent deploy within 3 seconds
+      lastDeploy = now;
+      var thisIndex = _this.sockets.indexOf(socket);
+      var color = thisIndex == 0 ? "red" : "blue";
+      io.emit('deploy', {color:color});
     });
   });
 }
@@ -24,7 +37,7 @@ var Room = function (name, lobby, io) {
 Room.prototype.toJSON = function(){
   return {
     name: this.name,
-    isPlaying: this.currentPlayerCount == this.playerCount
+    isPlaying: this.sockets.length == this.playerCount
   };
 }
 
