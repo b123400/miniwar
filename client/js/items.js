0d1c976fda067a659643a69d13fd462d9ee1add9
b123400 <b123400@gmail.com>
2015-04-12 21:06:02 +0800

    make siuming a Soldier subclass

--- client/js/items.js
+++ client/js/items.js
@@ -193,68 +193,13 @@ Soldier.objectForDeploy = function () {
 
 /*siuming*/
 var siuming = function(options) {
-  Item.apply(this, arguments);
-  this.speed = options.speed; // pixel per second
-  this.lastMove = Date.now();
-  this.lastAttack = Date.now();
-  this.target = Player.fromId(options.target).castle;
+  Soldier.apply(this, arguments);
 };
 
-siuming.prototype = Object.create(Item.prototype);
-
-siuming.prototype.animateSprite = function () {
-  var _this = this;
-  function animate () {
-    if (_this.hp <= 0) return; // if this thing is destroyed, stop
-
-    var targetLocation = _this.target.location;
-    var deltaX = targetLocation.x - _this.location.x;
-    var deltaY = targetLocation.y - _this.location.y;
-    var distance = Math.sqrt(Math.pow(deltaX,2) + Math.pow(deltaY,2));
-    var canMoveDistance = (Date.now() - _this.lastMove) / 1000 * _this.speed;
-    var percentage = canMoveDistance/distance;
-    if (percentage < 0) {
-      percentage = 0;
-    } else if (percentage > 1) {
-      percentage = 1;
-    }
-
-    var targetLocation = {
-      x : _this.location.x + deltaX * percentage,
-      y : _this.location.y + deltaY * percentage
-    };
-
-    var crashingItems = Stage.collisionItemsForItem(_this, targetLocation, _this.size);
-    crashingItems = crashingItems.filter(_this.shouldAttackItem);
-
-    if (!crashingItems.length) {
-      _this.location = targetLocation;
-    } else {
-      if (_this.owner === Player.me && Date.now() - _this.lastAttack > 1000) {
-        // yo lets attack!
-        crashingItems.forEach(function (item) {
-          _this.attack(item);
-        });
-      }
-    }
-
-    var sprite = _this.getSprite();
-    sprite.x = _this.location.x;
-    sprite.y = _this.location.y;
-    _this.lastMove = Date.now();
-
-    requestAnimationFrame(animate);
-  }
-  animate();
-}
+siuming.prototype = Object.create(Soldier.prototype);
 
 siuming.prototype.shouldAttackItem = function (item) {
-  return !(item instanceof siuming);
-}
-
-siuming.prototype.attack = function (anotherItem) {
-  Socket.attackItem(this, anotherItem, 10);
-  this.lastAttack = Date.now();
+  return !(item instanceof siuming); // attack every thing except siuming
 }
 
 siuming.createButtonSprite = function () {
@@ -283,8 +228,6 @@ siuming.objectForDeploy = function () {
 
 siuming.prototype.getImage = function () {
   return 'img/siuming.png';
-
-  
 };
 
 /*end of siuming*/
